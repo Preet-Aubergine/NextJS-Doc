a2ba1e4bed01da35e0c3378fac757feb6d253745
Preet_Abergine <preet@aubergine.co>
2025-02-06 17:57:22 +0530

    Invoice Management Completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css'
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'A modern and sleek invoice management application',
+}
+
+export const expermental_ppr = true;
 
 export default function RootLayout({
   children,
